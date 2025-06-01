4cec353e27d9c0a12178d4d298a1aae0a17713d5
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-06-01 17:26:29 +0200

    add BIO model

--- src/data.py
+++ src/data.py
@@ -11,3 +11,38 @@ def load():
     SENTIMENT_TOPIC = pd.read_csv(SENTIMENT_TOPIC_PATH, sep="\t")
 
     return (NER, SENTIMENT_TOPIC)
+
+
+def NER_data_to_sentence_array(NER: pd.DataFrame) -> list[tuple[str, list[tuple[str, str, str]]]]:
+    sentences = []
+
+    current_sentence_id = 0
+    sentence = ""
+    token_ids = []
+    tokens = []
+    BIO_NER_tags = []
+
+    for i, row in NER.iterrows():
+        sentence_id = row["sentence_id"]
+        if sentence_id != current_sentence_id:
+            sentences.append((sentence, list(zip(tokens, BIO_NER_tags, token_ids))))
+            current_sentence_id = sentence_id
+
+            sentence = ""
+            tokens = []
+            BIO_NER_tags = []
+            token_ids = []
+
+        token = row["token"]
+        token_id = row["token_id"]
+        BIO_NER_tag = row["BIO_NER_tag"]
+
+        if sentence == "":
+            sentence = token
+        else:
+            sentence += f" {token}"
+        tokens.append(token)
+        BIO_NER_tags.append(BIO_NER_tag)
+        token_ids.append(token_id)
+
+    return sentences
